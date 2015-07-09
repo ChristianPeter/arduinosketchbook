0f004e45e94e20d2f47699d6b06a304fbd3a7ffa
Christian Peter <peter.chr@web.de>
2015-07-09 23:00:50 +0200

    ir demos sonic and lcd

---
+++ IRLego3/IRLego3.ino
@@ -0,0 +1,163 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin irpin. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+const byte irpin = 3;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin irpin as an output.
+  pinMode(irpin, OUTPUT);
+  
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  Serial.println("ready.");
+  
+}
+
+// low bit  : HIGH: HIGHSIGus, LOWGAPus LOW
+// start bit: HIGH: HIGHSIGus, HIGHGAPus LOW
+// high bit : HIGH: HIGHSIGus, 553us LOW
+
+// proto:
+/*
+START : 1
+
+TOGGLE: 1
+ESC:    0
+CH:     00
+
+ADD:    0
+MODE:   001
+
+DATA:   0101
+
+LCRC: 0xF xor Nibble1 xor Nibble2 xor Nibble3
+= 3
+STOP: 1
+
+payload:
+1000 0001 0101 0011
+*/
+
+const unsigned long HIGHSIG = 156UL;
+
+const unsigned long HEADERGAP = 1014UL;
+
+const unsigned long LOWGAP = 260UL;
+
+const unsigned long HIGHGAP = 546UL;
+
+void sendNibble(byte nibble, int pin){
+  for (byte mask = 8; mask!=0; mask >>= 1){
+    if (mask & nibble) {
+      high(HIGHSIG, 38, pin);
+      wait(HIGHGAP);
+    }
+    else {
+      high(HIGHSIG, 38, pin);
+      wait(LOWGAP);
+    }
+  } 
+}
+
+void sendSeq(byte nib1, byte nib2, byte nib3, int pin){
+
+  unsigned long t = micros();
+
+  byte lcrc = 15 ^ nib1 ^ nib2 ^ nib3;
+  // start bit  
+  high(HIGHSIG, 38, pin);
+  wait(HEADERGAP);
+
+  sendNibble(nib1, pin);
+  sendNibble(nib2, pin);
+  sendNibble(nib3, pin);
+  sendNibble(lcrc, pin);  
+ 
+  // stop bit
+  high(HIGHSIG, 38, pin);
+  wait(HEADERGAP);
+  
+  
+  Serial.println(t);
+  // stop bit
+}
+
+
+void wait(unsigned int time){
+  unsigned long start = micros();
+  while(micros()-start<=time){
+  }
+}
+
+void high(unsigned int time, int freq, int pinLED){
+  int pause = (1000/freq/2)-4;
+  unsigned long start = micros();
+  while(micros()-start<=time){
+    digitalWrite(pinLED,HIGH);
+    delayMicroseconds(pause);
+    digitalWrite(pinLED,LOW);
+    delayMicroseconds(pause);
+  }
+}
+
+void acc(){
+  sendSeq(B0100, B0001, B1111, irpin);
+  delay(300);
+  sendSeq(B0100, B0010, B1111, irpin);
+  delay(300);
+  sendSeq(B0100, B0011, B1111, irpin);
+  delay(300);
+  sendSeq(B0100, B0100, B1111, irpin);
+  delay(300);
+  sendSeq(B0100, B0101, B1111, irpin);
+  delay(300);
+  
+  sendSeq(B0100, B0111, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B0111, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B0111, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B0011, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B0011, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B1011, B1001, irpin);
+  delay(300);
+  sendSeq(B0100, B1011, B1001, irpin);
+  delay(300);
+  
+  
+}
+
+void dec(){
+  sendSeq(B0100, B0001, B1111, irpin);
+  delay(300);
+  sendSeq(B0100, B1000, B1000, irpin);
+  delay(300);
+}
+// the loop function runs over and over again forever
+void loop() {
+  //1000 0001 0101 0011
+
+  //sendSeq(B1000, B0001, B0101, irpin);
+  //sendSeq(B0100, B0001, B1111, irpin);
+  acc();
+  delay(3000);
+  
+}
